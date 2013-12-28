5e9d5dcc308575b9c3b8b2cffa3300fb64540e04
Jacob <jacobaarthur@gmail.com>
2013-12-27 22:40:45 -0600

    Updated the way commands work, they work as an interface with an Init and Cmd functions.

--- app.go
+++ app.go
@@ -2,17 +2,22 @@ package clu
 
 import "errors"
 
-type CmdFunc func(Args)
+type Command interface {
+	Init(ArgConf)
+	Cmd(Args)
+}
+
+type Cmds map[string]Command
 
 type AppConf interface {
 	AddFlag(string, string, string)
 	AddVar(string, string, string, string)
-	AddCmd(string, CmdFunc)
+	AddCmd(string, Command)
 }
 
 type App struct {
 	Args
-	cmds map[string]CmdFunc
+	cmds Cmds
 }
 
 func newApp() *App {
@@ -22,22 +27,26 @@ func newApp() *App {
 	app.flags = make(map[string]*bool)
 	app.help = make(map[string]*string)
 	app.loosies = make([]string, 0)
-	app.cmds = make(map[string]CmdFunc)
+	app.cmds = make(Cmds)
 
 	return app
 
 }
 
-func (app *App) AddCmd(name string, fp CmdFunc) {
+func (app *App) AddCmd(name string, fp Command) {
 	app.cmds[name] = fp
 }
 
 func (app *App) Run() error {
 	if len(app.loosies) > 0 {
 		if fp := app.cmds[app.loosies[0]]; fp != nil {
-			app.loosies = app.loosies[1:]
-			fp(app.Args)
-			return nil
+			// app.loosies = app.loosies[1:]
+			// fp(app.Args)
+			// nApp := 
+			// return nil
+			a := newArgs()
+			fp.Init(a)
+			fp.Cmd(*a)
 		}
 	}
 
